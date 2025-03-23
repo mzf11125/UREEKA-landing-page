a9f560d7dcb14f7f65c2af57d55860ba4bc5ad2c
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2025-03-23 22:02:59 +0700

    remove useState

--- landing-page/src/components/page-components/form.tsx
+++ landing-page/src/components/page-components/form.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "../ui/button";
 
 const Form = () => {

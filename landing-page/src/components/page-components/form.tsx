b7311fe1b04a03fc7141828d474dfed0d4db5a15
mzf11125 <77999453+mzf11125@users.noreply.github.com>
2025-04-06 22:38:43 +0700

    fixed

--- landing-page/src/components/page-components/form.tsx
+++ landing-page/src/components/page-components/form.tsx
@@ -6,6 +6,11 @@ import { createClient } from "@supabase/supabase-js";
 // Initialize Supabase client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Supabase URL or Key is missing. Please check your environment variables."
+  );
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Check if Supabase client is initialized
